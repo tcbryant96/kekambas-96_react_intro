51b567603e3005b3d24cb2a068c31ceddc56dccb
Brian <bstanton773@gmail.com>
2022-08-30 15:09:16 -0500

    Refactor with new components for Racers and ButtonCounter

--- src/App.js
+++ src/App.js
@@ -1,58 +1,15 @@
-import { useState, useEffect } from 'react';
-import Button from "./components/Button";
+import ButtonCounter from './components/ButtonCounter';
 import Navbar from "./components/Navbar";
 import Racers from './components/Racers';
 
 function App(props) {
-    let buttons = [
-        {color: 'primary', step: 1},
-        {color: 'secondary', step: 10},
-        {color: 'success', step: 100},
-        {color: 'danger', step: 1000},
-    ]
-
-    // Set a state for count - initial state of 0 and setCount is function to change state value of count
-    const [count, setCount] = useState(0);
-    // Set a state for racers - initial state of [] and setRacers is function to change state value of racers
-    const [racers, setRacers] = useState([]);
-
-    const [season, setSeason] = useState(2022);
-    const [round, setRound] = useState(1)
-
-    // Create an effect -> function to execute after every render
-    useEffect(() => {
-        console.log('useEffect effect callback executed.')
-        fetch(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
-            .then(res => res.json())
-            .then(data => {
-                let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-                setRacers(racerStandings);
-            })
-    }, [season, round])
-
-    // Function to be executed when a button is clicked
-    function handleClick(step){
-        setCount(count + step);
-    };
-
-    // Function to be exectuted when the name form is submitteed
-    function handleRacerSubmit(e){
-        // Prevent default of refreshing page
-        e.preventDefault();
-        let newSeason = e.target.season.value;
-        let newRound = e.target.round.value;
-        setSeason(newSeason);
-        setRound(newRound);
-    }
 
     return (
         <>
             <Navbar name='Brian' city='Chicago'/>
             <div className='container'>
-                <h1 className='text-center'>Hello World</h1>
-                <h3 className='text-center'>Total: {count}</h3>
-                {buttons.map((b, i) => <Button color={b.color} step={b.step} key={i} handleClick={handleClick} />)}
-                <Racers handleRacerSubmit={handleRacerSubmit} racers={racers} />
+                <ButtonCounter />
+                <Racers />
             </div>
         </>
     )
--- src/components/Racers.jsx
+++ src/components/Racers.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Racers(props) {
     let tableHeaders = ['#', 'First', 'Last', 'Points', 'Wins', 'Nationality', 'Constructor']
+    // Set a state for racers - initial state of [] and setRacers is function to change state value of racers
+    const [racers, setRacers] = useState([]);
+
+    const [season, setSeason] = useState(2022);
+    const [round, setRound] = useState(1)
+
+    // Create an effect -> function to execute after every render
+    useEffect(() => {
+        console.log('useEffect effect callback executed.')
+        fetch(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
+            .then(res => res.json())
+            .then(data => {
+                let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+                setRacers(racerStandings);
+            })
+    }, [season, round])
+
+    // Function to be exectuted when the name form is submitteed
+    function handleRacerSubmit(e){
+        // Prevent default of refreshing page
+        e.preventDefault();
+        let newSeason = e.target.season.value;
+        let newRound = e.target.round.value;
+        setSeason(newSeason);
+        setRound(newRound);
+    }
+
     return (
         <div className='row py-3'>
             <h4 className="text-center">Driver Standings</h4>
-            <form onSubmit={props.handleRacerSubmit}>
+            <form onSubmit={handleRacerSubmit}>
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <input type="text" className='form-control' name="season" placeholder='Enter Season' />
@@ -27,7 +54,7 @@ export default function Racers(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {props.racers.map((racer, idx) => {   
+                    {racers.map((racer, idx) => {   
                         return (<tr key={idx}>
                             <th>{racer.position}</th>
                             <td>{racer.Driver.givenName}</td>
